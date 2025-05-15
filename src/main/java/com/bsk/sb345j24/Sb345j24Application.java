4cd87ffcaab92e5873f4daeff1264427805135d0
“sbiruduraju” <sbiruduraju@gmail.com>
2025-05-15 23:19:15 +0530

    adding files

---
+++ src/main/java/com/bsk/sb345j24/Sb345j24Application.java
@@ -0,0 +1,13 @@
+package com.bsk.sb345j24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb345j24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb345j24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsk/sb345j24/Sb345j24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsk.sb345j24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb345j24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
